14a6a75fe024017243e9a7816d1eeb7c7c33bab9
Mohamed Amr Abd El Fatah <muhamedamr241@gmail.com>
2023-05-19 15:59:27 +0300

    Adding GUI

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import numpy as np
+import pygame
 import math
+import sys
 import random
+import tkinter as tk
+from tkinter import ttk
 
 NUMBER_OF_ROWS = 6
 NUMBER_OF_COLUMNS = 7
 
 COMPUTER_TOKEN = 1
 AI_TOKEN = 2
+
+BLUE = (0, 153, 153)
+BABYBLUE = (137, 207, 240)
+PURPLE = (204, 0, 204)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+SQUARESIZE = 100
+width = NUMBER_OF_COLUMNS * SQUARESIZE
+height = (NUMBER_OF_ROWS + 1) * SQUARESIZE
+size = (width, height)
+RADIUS = int(SQUARESIZE / 2 - 5)
 
 
 def isValidPos(grid, col):
@@ -25,9 +42,11 @@ def getNextRow(grid, col):
         if grid[row][col] == 0:
             return row
 
+
 def checkWinner(grid, player):
     if count_streak(grid, player, 4) >= 1:
         return True
+
 
 def estimate(grid):
     center_count = 0
@@ -121,6 +140,7 @@ def diagonal_streak(row, col, grid, streak):
 
     return total
 
+
 def is_terminal_node(grid):
     return checkWinner(grid, COMPUTER_TOKEN) or checkWinner(grid, AI_TOKEN) or len(getValidPositions(grid)) == 0
 
@@ -161,6 +181,8 @@ def minimax(grid, depth, maximizingPlayer):
                 min_eval = new_score
                 column = col
         return column, min_eval
+
+
 def minimax_alpha_beta(grid, depth, alpha, beta, maximizingPlayer):
     valid_locations = getValidPositions(grid)
     if is_terminal_node(grid):
@@ -203,3 +225,96 @@ def minimax_alpha_beta(grid, depth, alpha, beta, maximizingPlayer):
             if alpha >= beta:
                 break
         return column, min_eval
+
+
+def drawGrid(screen, grid):
+    for i in range(NUMBER_OF_COLUMNS):
+        for j in range(NUMBER_OF_ROWS):
+            pygame.draw.rect(screen, BLUE, (i * SQUARESIZE, j * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BABYBLUE, (
+                int(i * SQUARESIZE + SQUARESIZE / 2), int(j * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+    for i in range(NUMBER_OF_COLUMNS):
+        for j in range(NUMBER_OF_ROWS):
+            if grid[j][i] == COMPUTER_TOKEN:
+                pygame.draw.circle(screen, PURPLE, (
+                    int(i * SQUARESIZE + SQUARESIZE / 2), height - int(j * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            elif grid[j][i] == AI_TOKEN:
+                pygame.draw.circle(screen, YELLOW, (
+                    int(i * SQUARESIZE + SQUARESIZE / 2), height - int(j * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+    pygame.display.update()
+
+
+# Define the levels and algorithms
+levels = ["Easy", "Medium", "Hard"]
+algorithms = ["Minimax", "Alpha-Beta"]
+opponents = ["AI", "Human", "Random"]
+
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Algorithm Selection")
+window.geometry("800x800")
+
+# Variables to store the selected level and algorithm
+selected_level = tk.StringVar()
+selected_algorithm = tk.StringVar()
+selected_opponents = tk.StringVar()
+
+
+# Function to handle level selection
+def select_level(event):
+    selected_level.set(level_combobox.get())
+
+
+# Function to handle algorithm selection
+def select_algorithm(event):
+    selected_algorithm.set(algorithm_combobox.get())
+
+
+def select_opponent(event):
+    selected_opponents.set(opponents_combobox.get())
+
+
+# Create the level label and drop-down list
+level_label = ttk.Label(window, text="Select Level:")
+level_label.pack(pady=10)
+level_combobox = ttk.Combobox(window, values=levels, textvariable=selected_level)
+level_combobox.pack()
+level_combobox.bind("<<ComboboxSelected>>", select_level)
+
+# Create the algorithm label and drop-down list
+algorithm_label = ttk.Label(window, text="Select Algorithm:")
+algorithm_label.pack(pady=10)
+algorithm_combobox = ttk.Combobox(window, values=algorithms, textvariable=selected_algorithm)
+algorithm_combobox.pack()
+algorithm_combobox.bind("<<ComboboxSelected>>", select_algorithm)
+
+# Create the algorithm label and drop-down list
+opponents_label = ttk.Label(window, text="Select opponent:")
+opponents_label.pack(pady=10)
+opponents_combobox = ttk.Combobox(window, values=opponents, textvariable=selected_opponents)
+opponents_combobox.pack()
+opponents_combobox.bind("<<ComboboxSelected>>", select_opponent)
+
+
+# Function to print the selected level and algorithm
+def print_selection():
+    level = selected_level.get()
+    algorithm = selected_algorithm.get()
+    opp = selected_opponents.get()
+    if level == 'Easy':
+        depth = 1
+    elif level == 'Medium':
+        depth = 3
+    else:
+        depth = 4
+    window.destroy()
+    #execute(depth, algorithm, opp)
+
+
+# Create the button to print the selected level and algorithm
+print_button = ttk.Button(window, text="Start Game", command=print_selection)
+print_button.pack(pady=20)
+
+# Run the Tkinter event loop
+window.mainloop()
